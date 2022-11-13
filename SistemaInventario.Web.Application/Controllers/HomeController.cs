c766e38b0d5f5e29b135b6c7b88acfb49a7035cb
Juan Jose Henao <j.henao@crisolweb.net>
2022-11-13 12:10:31 -0500

    ADD: DbContext y la cadena de conexión a base de datos

--- SistemaInventario.Web.Application/Controllers/HomeController.cs
+++ SistemaInventario.Web.Application/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SistemaInventario.Web.Application.DataContext;
 using SistemaInventario.Web.Application.Models;
 using System.Diagnostics;
 
@@ -7,10 +9,12 @@ namespace SistemaInventario.Web.Application.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly SistemaInventarioDBContext dbContext;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, SistemaInventarioDBContext _dbContext)
         {
             _logger = logger;
+            dbContext = _dbContext;
         }
 
         public IActionResult Index()
--- SistemaInventario.Web.Application/Program.cs
+++ SistemaInventario.Web.Application/Program.cs
@@ -1,7 +1,18 @@
+using Microsoft.EntityFrameworkCore;
+using SistemaInventario.Web.Application.DataContext;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+//Accediendo a la conexion declarada en el appsettings
+//SistemaInventarioDBContext se pone a dispocision para todo el sistema
+
+ConfigurationManager configuration = builder.Configuration;
+
+var connectionSQL = configuration.GetConnectionString("ConexionSQL");
+builder.Services.AddDbContext<SistemaInventarioDBContext>(options => options.UseSqlServer(connectionSQL));
 
 var app = builder.Build();
 
